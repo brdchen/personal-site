cf914fcf7cbbc3bb9b31a2b7a2df07515f397ccf
Bradford Chen <bradchen@bradfords-mbp.attlocal.net>
2023-06-21 21:29:07 -0700

    add projects to Projects page, make nav bar highlight on select, and make pages a bit more mobile friendly

---
+++ src/app/projects/strings.tsx
@@ -0,0 +1,40 @@
+export const strings = {
+    dubhacks: {
+        title: 'DubHacks Registration Form',
+        preDubhacksDescription: `As an executive organizer for `,
+        dubhacksText: 'DubHacks',
+        postDubhacksDescription: `, the largest student-run Hackathon in the PNW, I helped build the DubHacks signup form on top of `,
+        quillText: 'Quill',
+        postQuillDescription: ', an open-source Hackathon registration and management system.',
+        link: 'https://github.com/JoshiAbhishek/dubhacks18#quill',
+        dubhacksLink: 'https://dubhacks.co',
+        quillLink: 'https://github.com/techx/quill',
+        tech: 'Angular, JavaScript, MongoDB, Heroku'
+    },
+    lofi: {
+        title: 'Lofi.me',
+        description: `A Discord bot which hooks up to YouTube and Spotify to stream music and video audio in a user's current voice channel. API is accessed by typing certain commands into the Discord server chat.`,
+        link: 'https://github.com/Jyntaro/lofi.me',
+        tech: 'Node.js'
+    },
+    hyperspeed: {
+        title: 'Hyperspeed Fighters',
+        description: `A simple mouse accuracy space shooter time-attack game where a player's score is determined by the amount of enemies hit. Comes with sounds, graphics, and simple animations.`,
+        link: 'https://github.com/Jyntaro/Hyperspeed-Fighters',
+        processingLink: 'https://processing.org',
+        preProcessingTech: 'Java, ',
+        processingText: 'Processing',
+        postProcessingTech: ' (graphical library using Java)',
+    },
+    paxos: {
+        title: 'Paxos-based Key/value service',
+        description: 'My favorite project from my favorite university course! A Paxos protocol-based distributed system that manages replication of a kev/value store. Supports Put, Append, and Get RPC operations.',
+        link: 'https://gitlab.cs.washington.edu/dwoos/452-labs/-/wikis/Lab-3',
+        tech: 'Java'
+    },
+    threegle: {
+        title: '333gle',
+        description: 'A multithreaded search engine with inverted index and file crawler built from scratch in C/C++. Supports querying from a UI clone of Google.',
+        tech: 'C, C++, WebSockets'
+    },
+}
